a742e306495c08f3ec0f4ffccc885e7837895f07
jackpot6663 <37533840+jackpot66663@users.noreply.github.com>
2021-03-15 21:02:30 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Home></Home>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Home from '@/components/Home'
+
+export default {
+  name: 'App',
+  components:{
+    Footer,
+    Header,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
